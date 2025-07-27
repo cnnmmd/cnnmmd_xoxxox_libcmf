0578b1bd943051288768c5094d6a794af11587d7
azsgit <10238943+azsgit@users.noreply.github.com>
2025-07-27 22:09:30 +0900

    wip

---
+++ export/dyn/xoxxox_libcmf/__init__.py
@@ -0,0 +1,1 @@
+from .libcmf import AppCmf
---
+++ export/dyn/xoxxox_libcmf/libcmf.py
@@ -0,0 +1,44 @@
+#---------------------------------------------------------------------------
+# 参照
+
+import glob
+import json
+from xoxxox.shared import PrcFlw
+from xoxxox.libmid import LibMid
+from xoxxox.params import Config
+from xoxxox.params_cmf import CnfCmf
+
+#---------------------------------------------------------------------------
+# 処理：ＧＵＩワークフロー環境向け
+
+class AppCmf:
+
+  # サーバのアドレス群の辞書を返す（文字列（JSON形式））
+  @staticmethod
+  def dicsrv():
+    dicsrv=PrcFlw.dicsrv()
+    datres = json.dumps(dicsrv).encode("utf-8")
+    return datres
+
+  # ワークフローのオプション群の辞書を返す（文字列（JSON形式））
+  @staticmethod
+  def diccmf():
+    diccmf=AppCmf.getcmf()
+    datres = json.dumps(diccmf).encode("utf-8")
+    return datres
+
+  def getcmf():
+    diccnf = {}
+    lstpth = glob.glob(Config.dircnf + "/" + CnfCmf.glbcmf)
+    for pthcnf in lstpth:
+      with open(pthcnf, "r") as f:
+        d = json.load(f)
+        for key, lst in d.items():
+          if key in diccnf:
+            diccnf[key].extend(lst)
+          else:
+            diccnf[key] = lst
+    return diccnf
+
+LibMid.dicprc.append({"key": "xoxxox.AppCmf.diccmf", "frm": "LibMid.plugin['xoxxox_libcmf'].AppCmf.diccmf()", "syn": True})
+LibMid.dicprc.append({"key": "xoxxox.AppCmf.dicsrv", "frm": "LibMid.plugin['xoxxox_libcmf'].AppCmf.dicsrv()", "syn": True})
